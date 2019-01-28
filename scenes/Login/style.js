9218b55770173c9a0302153858b7731b9d341f47
Vinicius <viniciuspsilvas@gmail.com>
2019-01-28 14:29:08 +1000

    Remove old code

---
+++ scenes/Login/style.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+export default Styles = StyleSheet.create({
+    button: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 30,
+    },
+    label: {
+        color: 'red',
+    },
+    error: {
+        color: 'green',
+    },
+});
