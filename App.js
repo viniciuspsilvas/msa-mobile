895bfa5f3fbc9bb0731b4a68d3e285a1b0a492ac
Vinnie <viniciuspsilvas@gmail.com>
2019-04-10 23:11:18 +1000

    RootReducer was moved. App.js was merged.

--- App.js
+++ App.js
@@ -1,6 +1,59 @@
-import  StorybookUIRoot from './storybook';
 
-import  App from './App.main.js';
+import React from 'react';
+import AppNavigation from './app/AppNavigation'
+//import { IS_STORYBOOK_ENABLED } from 'react-native-dotenv'
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { createLogger } from "redux-logger";
+import rootReducer from "./app/RootReducer";
+//import  StorybookUIRoot from './storybook';
 
-export default App;
+const logger = createLogger();
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(thunk, logger)
+));
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+            user: {},
+            isLoading: true
+        };
+    }
+
+    // Workaround to solve the problem related to font 'Roboto_medium'
+    async componentWillMount() {
+        await Expo.Font.loadAsync({
+            Roboto: require("native-base/Fonts/Roboto.ttf"),
+            Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+            Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+        });
+
+        this.setState({ isLoading: false });
+    }
+
+    render() {
+
+        if (this.state.isLoading) {
+            return <Expo.AppLoading />;
+        }
+
+        return (
+            <Provider store={store}>
+                {/* {IS_STORYBOOK_ENABLED === 'true' ? (
+                    <StorybookUIRoot />
+                ) : (
+                )}  */}               
+                <AppNavigation />
+            </Provider>
+        );
+    }
+}
+
--- app/RootReducer.js
+++ app/RootReducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
-import loginReducer from "./src/scenes/Login/reducer";
-import messagesReducer from "./src/scenes/Messages/reducer";
-import attendanceReducer from "./src/scenes/Attendance/reducer";
+import loginReducer from "../src/scenes/Login/reducer"
+import messagesReducer from "../src/scenes/Messages/reducer";
+import attendanceReducer from "../src/scenes/Attendance/reducer";
 
 import { reducer as formReducer } from 'redux-form';
 
