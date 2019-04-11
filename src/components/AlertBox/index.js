c775036677ce81aa46d08ee4776a9a3abb578623
Vinnie <viniciuspsilvas@gmail.com>
2019-04-11 20:36:21 +1000

    AlertBox components was added.

---
+++ src/components/AlertBox/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Container, Toast } from "native-base";
+
+class AlertBox extends React.Component {
+
+
+    render() {
+        const {text, buttonText, duration = 3000} = this.props;
+
+        return (
+            <Container>
+                {Toast.show({
+                    text: text,
+                    buttonText: buttonText,
+                    duration: duration
+                })}}
+            </Container>
+        );
+    }
+}
+
+AlertBox.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default AlertBox;
