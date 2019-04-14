7a6e301f2809425783e685213294afe5bc1ca163
Vinnie <viniciuspsilvas@gmail.com>
2019-04-14 21:53:17 +1000

    BACKEND_PORT was removed.

--- config/config.js
+++ config/config.js
@@ -1,24 +1,20 @@
-import { BACKEND_URL, BACKEND_PORT } from 'react-native-dotenv'
-
-
-const FULL_BACKEND_URL = BACKEND_URL + (BACKEND_PORT ?  ":"+ BACKEND_PORT : "");
+import { BACKEND_URL } from 'react-native-dotenv'
 
 var backend = {
 
 	// Students API - '/api/Students'
-	students: FULL_BACKEND_URL + '/api/Students',
-	loginMoodle: FULL_BACKEND_URL + '/api/Students/loginMoodle',	
-	studentAdvices: FULL_BACKEND_URL + '/api/StudentAdvices',
-	getAttendance: FULL_BACKEND_URL + '/api/Students/getAttendance',
+	students: BACKEND_URL + '/api/Students',
+	loginMoodle: BACKEND_URL + '/api/Students/loginMoodle',	
+	studentAdvices: BACKEND_URL + '/api/StudentAdvices',
+	getAttendance: BACKEND_URL + '/api/Students/getAttendance',
 	
 
-	messages: FULL_BACKEND_URL + '/api/Messages',
-	//groupStudents: FULL_BACKEND_URL + '/groupStudents',
+	messages: BACKEND_URL + '/api/Messages',
+	//groupStudents: BACKEND_URL + '/groupStudents',
 }
 
 var config = {
-	backend_port: BACKEND_PORT,
-	backend_url: FULL_BACKEND_URL,
+	backend_url: BACKEND_URL,
 	backend: backend
 }
 
