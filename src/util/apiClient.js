5e3c93ead9e650f787ef4c2a09276934e509fa4b
Vinnie <viniciuspsilvas@gmail.com>
2019-11-21 07:13:13 +1000

    Fixed bugs

--- src/util/apiClient.js
+++ src/util/apiClient.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import { BACKEND_URL } from 'react-native-dotenv'
- 
+
 export const axiosInstance = axios.create({
     baseURL: BACKEND_URL,
     timeout: 15000,
