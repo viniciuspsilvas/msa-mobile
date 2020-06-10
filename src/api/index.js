e3cf9609526352e52fe00ebc31af9fb5d65ba5b3
Vinicius <viniciuspsilvas@gmail.com>
2020-06-10 15:22:38 +1000

    Fixing handle errors

--- src/api/index.js
+++ src/api/index.js
@@ -4,13 +4,13 @@ import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { setContext } from "apollo-link-context";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { onError } from "apollo-link-error";
+import { Alert } from 'react-native'
 
 const httpLink = createHttpLink({ uri: BACKEND_URL });
-
 const authLink = setContext(async (_, { headers }) => {
 
   const student = JSON.parse(await AsyncStorage.getItem('STUDENT_MSA'))
-
   if (student && student.token) {
     return {
       headers: {
@@ -23,11 +23,24 @@ const authLink = setContext(async (_, { headers }) => {
       headers: { ...headers }
     };
   }
+});
 
+const link = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) => {
+      Alert.alert(message);
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    });
+  if (networkError) {
+    Alert.alert(networkError);
+    console.log(`[Network error]: ${networkError}`)
+  };
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: authLink.concat(link).concat(httpLink),
   cache: new InMemoryCache()
 });
 
